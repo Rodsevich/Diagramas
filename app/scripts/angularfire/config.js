6904ab9c2b1e12d8d6af85b31ae7367d3f431dcb
Rodsevich <neton_12@hotmail.com>
2015-05-09 22:01:14 -0300

    limpieza

--- app/scripts/angularfire/config.js
+++
@@ -1,5 +0,0 @@
-angular.module('firebase.config', [])
-  .constant('FBURL', 'https://shining-inferno-8432.firebaseio.com')
-  .constant('SIMPLE_LOGIN_PROVIDERS', ['github'])
-
-  .constant('loginRedirectPath', '/');
